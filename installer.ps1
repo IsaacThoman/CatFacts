478dd7749b4fd347512899faaf123ede215498f1
Isaac Thoman <isaacthoman@outlook.com>
2020-03-31 22:06:55 -0400

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,8 +1,5 @@
 Invoke-WebRequest -Uri "https://github.com/Excrement/CatFacts/raw/master/catfacts.exe" -OutFile "..\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\CatFacts.exe"
-
-
-cd C:/
-mkdir catfacts
+mkdir C:/catfacts
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Excrement/CatFacts/master/facts.txt" -OutFile "C:/catfacts/facts.txt"
-
-start "..\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\CatFacts.exe"
+cd "..\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+start catfacts.exe
