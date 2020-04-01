d3585a80cf987a5a0ec41323c2495541ca541930
Isaac Thoman <isaacthoman@outlook.com>
2020-03-31 21:43:26 -0400

    Create cats.ps1

---
+++ cats.ps1
@@ -0,0 +1,19 @@
+$messagelimit = 254 
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Rate   = -2  # -10 to 10; -10 is slowest, 10 is fastest
+sleep 15
+$textline = 0
+function fact {
+$textline = $textline + 1
+$message = (gc C:/cats.txt | select-object -Index $textline) | Out-String
+write-output $message
+$speak.Speak($message)
+sleep 1000
+if($textline -lt $messagelimit) {
+fact
+}
+$textline = 0
+fact
+}
+fact
